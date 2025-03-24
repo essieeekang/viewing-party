1468d4947e9a5041fe1065c072e92a893a0048cf
Aigerim Kalygulova <kalygulovas@gmail.com>
2025-03-24 11:21:48 -0700

    implements add_to_watchlist and passes 2 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -15,6 +15,12 @@ def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
     return user_data
 
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+    
+    
+    
     
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
