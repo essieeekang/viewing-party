755607c0547d6c0d2b3af3d1e36efa157ce9378b
Esther Kang <estherkang.j@gmail.com>
2025-03-26 14:24:42 -0700

    implements get_rec_from_favorites, passes all tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -114,4 +114,19 @@ def get_available_recs(user_data):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+def get_new_rec_by_genre(user_data):
+    recommendations = []
+    fav_genre = get_most_watched_genre(user_data)
+    friends_unique_movies = get_friends_unique_watched(user_data)
 
+    for movie in friends_unique_movies:
+        if movie["genre"] == fav_genre:
+            recommendations.append(movie)
+
+    return recommendations
+
+def get_rec_from_favorites(user_data):
+    friends_movies = get_friends_movies_list_no_duplicates(user_data["friends"])
+    recommended = get_unique_list_1_movies(user_data["favorites"], friends_movies)
+
+    return recommended
